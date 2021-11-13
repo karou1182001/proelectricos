c94d03bd7d3be31150e5d5b468ad83c93ca0f67d
Luis Ángel de Ávila Bula <luis.deavila@urosario.edu.co>
2021-11-13 09:33:38 -0500

    Fixed multiple + on job widget

--- proelectricos-main/p1/lib/domain/controller/workpage_controller.dart
+++ proelectricos-main/p1/lib/domain/controller/workpage_controller.dart
@@ -2,9 +2,7 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:p1/ui/widgets/menu_trabajo/job_widget.dart';
 
-
 class WorkPageController extends GetxController {
-
   final RxList<Widget> _joblist = <Widget>[].obs;
 
   RxList<Widget> get joblist => _joblist;
@@ -12,8 +10,14 @@ class WorkPageController extends GetxController {
   void init() async {
     addAddJobButton();
   }
+
   //Esta función añade el botón que permite añadir nuevos trabajos
   void addAddJobButton() {
+    if (_joblist.isNotEmpty) {
+      if (_joblist.last is IconButton) {
+        return;
+      }
+    }
     _joblist.add(IconButton(
       onPressed: () => {addJob()},
       icon: const Icon(Icons.add),
@@ -28,5 +32,4 @@ class WorkPageController extends GetxController {
     //Esto añade el botón debajo de la lista
     addAddJobButton();
   }
-
 }
--- proelectricos-main/p1/lib/ui/widgets/menu_trabajo/job_widget.dart
+++ proelectricos-main/p1/lib/ui/widgets/menu_trabajo/job_widget.dart
@@ -21,50 +21,5 @@ class JobWidget extends StatelessWidget {
             builder: (BuildContext context) => const MenuOptionsScreen()))
       },
     );
-
-    /*height: 100,
-        margin: const EdgeInsets.only(left: 30, right: 60),
-        child: Row(
-            //mainAxisAlignment: MainAxisAlignment.start,
-            //crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Indicator
-              Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: proElectricosBlue),
-                      color: const Color(0xffefefef)),
-                  child: IconButton(
-                      onPressed: () => {},
-                      icon: const Icon(Icons.check_box,
-                          color: proElectricosBlue))),
-              // Text("$jobName"),
-              // IconButton(
-              //     onPressed: () {},
-              //     icon: Icon(Icons.skip_next, color: ProElectricosBlue))
-              Expanded(
-                  child: Container(
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.grey)),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                          margin: const EdgeInsets.only(left: 8),
-                          child: Text(jobName)),
-                      IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder:
-                                      (context) => //Encargamos al builder que cambie el contexto a la página de menú general
-                                          const MenuOptionsScreen()));
-                        },
-                        icon: const Icon(Icons.skip_next,
-                            color: proElectricosBlue),
-                      ),
-                    ]),
-              ))
-            ])*/
   }
 }
